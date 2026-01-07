a8db561693f261cc0730f779f1d2f64b17c49766
Yong He <yonghe@outlook.com>
2026-01-07 11:30:46 -0800

    pull more infra from slang.

---
+++ example-base/core/slang-rich-diagnostics-render.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "slang-diagnostic-sink.h"
+#include "slang-list.h"
+#include "slang-source-loc.h"
+#include "slang-string.h"
+
+namespace Slang
+{
+
+struct SourceManager;
+
+struct DiagnosticSpan
+{
+    Slang::SourceRange range;
+    String message;
+};
+
+struct DiagnosticNote
+{
+    String message;
+    DiagnosticSpan span;
+};
+
+//
+// A struct capable of representing any diagnostic we want to display
+//
+struct GenericDiagnostic
+{
+    Int64 code;
+    Severity severity;
+    String message;
+    DiagnosticSpan primarySpan;
+    List<DiagnosticSpan> secondarySpans;
+    List<DiagnosticNote> notes;
+};
+
+struct DiagnosticRenderOptions
+{
+    bool enableTerminalColors = false;
+    bool enableUnicode = false;
+};
+
+String renderDiagnostic(
+    DiagnosticSink::SourceLocationLexer sll,
+    SourceManager* sm,
+    DiagnosticRenderOptions opts,
+    const GenericDiagnostic& diag);
+
+#define SLANG_ENABLE_DIAGNOSTIC_RENDER_UNIT_TESTS 0
+#ifdef SLANG_ENABLE_DIAGNOSTIC_RENDER_UNIT_TESTS
+int slangRichDiagnosticsUnitTest(int argc, char* argv[]);
+#endif
+
+} // namespace Slang
